d447fcc835a273fc76168fee6aad85bbdedf22df
Dominik Sander <git@dsander.de>
2016-11-15 14:00:40 +0100

    Replace Sidekiq::Fetcher::TIMEOUT with Sidekiq::BasicFetch::TIMEOUT

--- lib/sidekiq-queue-pause.rb
+++ lib/sidekiq-queue-pause.rb
@@ -52,7 +52,7 @@ module Sidekiq
         if qcmd.size > 1
           retrieve_work_for_queues qcmd
         else
-          sleep(Sidekiq::QueuePause.retry_after || Sidekiq::Fetcher::TIMEOUT)
+          sleep(Sidekiq::QueuePause.retry_after || Sidekiq::BasicFetch::TIMEOUT)
           nil
         end
       end
@@ -65,7 +65,7 @@ module Sidekiq
       def unpaused_queues_cmd
         queues = queues_cmd
         queues.reject do |q|
-          q != Sidekiq::Fetcher::TIMEOUT &&
+          q != Sidekiq::BasicFetch::TIMEOUT &&
             Sidekiq::QueuePause.paused?(q.gsub('queue:', ''), Sidekiq::QueuePause.process_key)
         end
       end
